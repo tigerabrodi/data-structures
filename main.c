6866b1ff7b52605edfe2319c7580df582a45d360
Tiger Abrodi <tigerabrodi@gmail.com>
2023-09-10 06:41:11 +0200

    print

--- main.c
+++ main.c
@@ -10,6 +10,14 @@ void print_list(Node *head);
 
 int main()
 {
+	Node a, b, c;
+	a.value = 1;
+	b.value = 2;
+	c.value = 3;
+	a.next = &b;
+	b.next = &c;
+	c.next = NULL;
+	print_list(&a);
 
 	return 0;
 }
