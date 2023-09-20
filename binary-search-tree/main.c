dd82c058e2cf0cf7603feb5a8e1c5cca8ebea5be
Tiger Abrodi <tigerabrodi@gmail.com>
2023-09-20 05:42:48 +0200

    commit

--- binary-search-tree/main.c
+++ binary-search-tree/main.c
@@ -83,31 +83,28 @@ void insert(BST *tree, int value)
 
 Node *search(BST *tree, int value)
 {
-	// 1. Create a Node pointer called 'current' and set it to the root of the tree.
-	// 2. Use a while loop to navigate through the tree as long as 'current' is not NULL.
-	// 3. In each iteration, compare 'value' with the value of 'current':
-	//    a. If they're equal, return 'current' (you've found the node).
-	//    b. If 'value' is less than the value of 'current', set 'current' to its 'left' child.
-	//    c. Otherwise, set 'current' to its 'right' child.
-	// 4. If you exit the loop without returning, this means the value is not in the tree. Return NULL.
+	Node *current = tree->root;
+	while (current != NULL)
+	{
+		if (value == current->value)
+		{
+			return current;
+		}
+		else if (value < current->value)
+		{
+			current = current->left;
+		}
+		else
+		{
+			current = current->right;
+		}
+	}
+
+	return NULL;
 }
 
 void delete(BST *tree, int value)
 {
-	// 1. You'll need two pointers: one called 'current' to navigate and another called 'parent' to keep track of the parent node. Initialize both to NULL.
-	// 2. First, find the node (and its parent) to be deleted using a loop similar to the search function.
-	// 3. Once you find the node:
-	//    a. If it's a leaf node (both left and right are NULL):
-	//       - Check if it's a left or right child of its parent and set the corresponding pointer of the parent to NULL.
-	//       - Free the node.
-	//    b. If the node has only one child (either left or right, but not both):
-	//       - Bypass the node: if the node is the left child of its parent, set the left child of the parent to the child of the node.
-	//       - If the node is the right child of its parent, set the right child of the parent to the child of the node.
-	//       - Free the node.
-	//    c. If the node has two children:
-	//       - Find either the in-order predecessor or the in-order successor of the node.
-	//       - Copy its value to the node.
-	//       - Then, delete the in-order predecessor/successor.
 }
 
 void inorder_traversal(Node *node)
