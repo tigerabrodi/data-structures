8697555ccac3a6980794312a14c726aa196dadce
Tiger Abrodi <tigerabrodi@gmail.com>
2023-10-10 07:48:55 +0200

    setup

--- adjacency-matrix/main.c
+++ adjacency-matrix/main.c
@@ -16,6 +16,81 @@ bool isEdge(Graph *graph, int vertex1, int vertex2);	 // Checks if an edge exist
 void printGraph(Graph *graph);				 // Prints the adjacency matrix
 void freeGraph(Graph *graph);				 // Cleans up memory used by the graph
 
+// Create a graph with the given number of vertices.
+Graph *createGraph(int numVertices)
+{
+	// Allocate space for the graph's structure
+	// - Use 'malloc' to dynamically allocate memory for the graph.
+	// - Handle any memory allocation failure (if 'malloc' returns NULL).
+
+	// Set the number of vertices in the graph structure.
+	// - Assign the 'numVertices' parameter to the 'numVertices' attribute of the graph.
+
+	// Allocate memory for the adjacency matrix.
+	// - Use a loop to create rows (i.e., pointers to int arrays).
+	//   Loop from 0 to numVertices-1.
+	//   - For each iteration:
+	//     - Allocate an array of integers for that row.
+	//     - Ensure memory allocation succeeded.
+	//     - Initialize all integers in this new array to 0 using a nested loop or memset.
+
+	// Return the newly created graph pointer.
+}
+
+void addEdge(Graph *graph, int vertex1, int vertex2)
+{
+	// Establish a connection (edge) between vertex1 and vertex2.
+	// - Access the adjacency matrix inside the graph.
+	// - Set the value at matrix position [vertex1][vertex2] to 1.
+
+	// Because this is an undirected graph:
+	// - Also set the value at position [vertex2][vertex1] to 1.
+}
+
+void removeEdge(Graph *graph, int vertex1, int vertex2)
+{
+	// Remove the connection (edge) between vertex1 and vertex2.
+	// - Access the adjacency matrix inside the graph.
+	// - Set the value at matrix position [vertex1][vertex2] to 0.
+
+	// Since the graph is undirected:
+	// - Also set the value at matrix position [vertex2][vertex1] to 0.
+}
+
+bool isEdge(Graph *graph, int vertex1, int vertex2)
+{
+	// Check if an edge exists between vertex1 and vertex2.
+	// - Access the adjacency matrix at the position [vertex1][vertex2].
+	// - If the value at this position is 1:
+	//   - An edge exists: return 'true'.
+	// - Otherwise:
+	//   - No edge exists: return 'false'.
+}
+
+void printGraph(Graph *graph)
+{
+	// Print the adjacency matrix row by row.
+	// - Loop through the rows from 0 to graph->numVertices-1.
+	//   - For each row:
+	//     - Use another loop to iterate through the columns from 0 to graph->numVertices-1.
+	//       - Print the matrix value at the current row and column.
+	//       - Add a space after each value for formatting.
+	//     - After finishing a row (when the inner loop completes), print a newline.
+}
+
+void freeGraph(Graph *graph)
+{
+	// Free all allocated memory associated with the graph.
+
+	// Begin by freeing the adjacency matrix:
+	// - Loop through the rows of the matrix (from 0 to graph->numVertices-1).
+	//   - For each row:
+	//     - Free the memory associated with that row.
+
+	// Once the matrix is freed:
+	// - Free the graph structure itself.
+}
+
 int main()
 {
 	int numVertices = 5; // Example size
